e6d80f094707fd246548440ac4bda8b1142e82b9
Edith Wanguba <wanguamor@gmail.com>
2023-09-29 14:42:26 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code here!
+// Create a new 'h1' element with the ID 'victory'
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+
+// Set the text content of the new header
+newHeader.textContent = 'YOUR-NAME is the champion';
+
+// Append the new header to the document body (or any other element you prefer)
+document.body.appendChild(newHeader);
+
